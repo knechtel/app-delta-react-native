bd269a167af177b0792dc2d83365fb271848ecb4
knechtel <maiquelknechtel@gmail.com>
2022-02-03 20:44:09 -0300

    Adicionando campos ao FormEquipment.

--- src/components/FormEquipment.js
+++ src/components/FormEquipment.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {StyleSheet, TextInput} from 'react-native';
+import {StyleSheet, TextInput, Text, View} from 'react-native';
 import {Button, SafeAreaView, useColorScheme} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RadioButton} from 'react-native-paper';
 import axios from 'axios';
 
-import {CREATE_CLIENT} from '../util/urls';
+import {CREATE_CLIENT, CREATE_EQUIPMENT} from '../util/urls';
 
 const FormEquipment = () => {
   const [name, setName] = React.useState();
@@ -18,8 +19,16 @@ const FormEquipment = () => {
   const [autorizado, setAutorizado] = React.useState(false);
   const [entregue, setEntregue] = React.useState(false);
 
-  const createClient = () => {
-    axios({
+  const createClient = async () => {
+    var idClient;
+    var aparelhoEntregue = null;
+    if (entregue == true) {
+      const d = new Date();
+      d.getTime();
+      aparelhoEntregue = d.toISOString().substring(0, 10);
+    }
+    console.log('Valor esperado =  ' + entregue);
+    await axios({
       method: 'post',
       url: CREATE_CLIENT,
       headers: {
@@ -29,6 +38,22 @@ const FormEquipment = () => {
         name: name,
         email: email,
         cpf: cpf,
+      },
+    }).then(response => {
+      idClient = response.data.id;
+      console.log('idClient  = ' + idClient + ' entregue ' + aparelhoEntregue);
+    });
+
+    await axios({
+      method: 'post',
+      url: CREATE_EQUIPMENT,
+      headers: {
+        'Content-type': 'application/json',
+      },
+      data: {
+        idClient: idClient,
+        brand: brand,
+        entregue: aparelhoEntregue,
       },
     });
   };
@@ -68,21 +93,37 @@ const FormEquipment = () => {
           value={checked}>
           <RadioButton.Item label="Pronto" value="first" />
         </RadioButton.Group>
-        <RadioButton.Group
-          onValueChange={value => setAutorizado(value)}
-          value={checked}>
-          <RadioButton.Item label="Autorizado" value="first" />
-        </RadioButton.Group>
-        <RadioButton.Group
-          onValueChange={value => setEntregue(value)}
-          value={checked}>
-          <RadioButton.Item label="Entregue" value="first" />
-        </RadioButton.Group>
+        <View style={styles1.checkboxContainer}>
+          <CheckBox
+            style={styles1.checkbox}
+            value={entregue}
+            onValueChange={value => setEntregue(value)}
+          />
+          <Text style={styles1.label}>Aparelho entregue!</Text>
+        </View>
         <Button title="Enviar" onPress={createClient} />
       </SafeAreaView>
     </>
   );
 };
+
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  checkbox: {
+    alignSelf: 'center',
+  },
+  label: {
+    margin: 8,
+  },
+});
 
 const styles = StyleSheet.create({
   sectionContainer: {
