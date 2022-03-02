6790b50abad711e5baf4b76959666bb400eef61a
knechtel <maiquelknechtel@gmail.com>
2022-03-02 19:58:01 -0300

    Adicionando recurso a tela de equipamentos.

--- src/components/ListEquipment.js
+++ src/components/ListEquipment.js
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
 import {Text} from 'react-native';
+import axios from 'axios';
+
+import {FIND_ALL_EQUIPMENT_BY_CLIENT} from '../util/urls';
 class ListEquipment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      equipment: [],
+      refreshing: false,
+    };
+  }
+  componentDidMount() {
+    axios.get(FIND_ALL_EQUIPMENT_BY_CLIENT).then(response => {
+      this.setState({
+        equipment: response.data,
+      });
+    });
+  }
+
   render() {
     return (
       <>
