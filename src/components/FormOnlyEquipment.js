44dbd271dcf835638d3e43e8533fd5faefad2cf5
knechtel <maiquelknechtel@gmail.com>
2022-03-04 00:57:34 -0300

    Adicionandi checkbox pronto.

--- src/components/FormOnlyEquipment.js
+++ src/components/FormOnlyEquipment.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import {Button, SafeAreaView, TextInput, StyleSheet} from 'react-native';
+import {Button, SafeAreaView, TextInput, StyleSheet, View} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 import {Text} from 'react-native';
 class FormOnlyEquipment extends Component {
-  state = {serial: 'hello world', model: '', defeito: ''};
+  state = {serial: 'hello world', model: '', defeito: '', pronto: false};
   componentDidMount() {
     const {navigation, route} = this.props;
     console.log('doChamada()');
@@ -16,6 +17,7 @@ class FormOnlyEquipment extends Component {
     const setSerial = serial => this.setState(serial);
     const setModel = model => this.setState(model);
     const setDefeito = defeito => this.setState(defeito);
+    const setPronto = pronto => this.setState(pronto);
     return (
       <>
         <SafeAreaView>
@@ -37,12 +39,56 @@ class FormOnlyEquipment extends Component {
             onChangeText={defeito => setDefeito(defeito)}
             defaultValue={state.defeito}
           />
-
+          <View style={styles1.checkboxContainer}>
+            <CheckBox
+              style={styles1.checkbox}
+              value={this.pronto}
+              onValueChange={value => setPronto(value)}
+            />
+            <Text style={styles1.label}>Aparelho pronto!</Text>
+          </View>
           <Button title="Enviar" onPress={this.saveEquipment} />
         </SafeAreaView>
       </>
     );
   }
 }
+
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  checkbox: {
+    alignSelf: 'center',
+  },
+  label: {
+    margin: 8,
+  },
+});
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 export default FormOnlyEquipment;
